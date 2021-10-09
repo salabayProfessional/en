e6d33d2e544cf79c6efc519452fe9e3cbe063401
salabayProfessional <85737310+salabayProfessional@users.noreply.github.com>
2021-10-09 03:06:58 +0300

    update

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './component/app/app';
-
-describe('renders learn react link', () => {
-  it("", () => {
-    render(<App />);
-  });
-});
